3b63d11de558b382f81f6d5032d3ec08204e1f19
sebastian nor <sebanor03@gmail.com>
2022-11-10 17:32:47 -0300

    clase 19

---
+++ clase 19/parallax/js/codigo.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $window = $(window);
+
+    $('#intro').height($window.height());
+    $window.scroll(function () {
+        $('div.background').each(function (index, item) {
+            let $scroll = $(item);
+            let yPos = -($window.scrollTop() / $scroll.data('speed'));
+            let coords = '50% ' + yPos + 'px';
+            $scroll.css({ backgroundPosition: coords });
+        })
+    })
+
+})
