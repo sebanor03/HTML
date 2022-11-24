beeca22e76f1759f7fb3cf66d91cfa021a8275bf
sebastian nor <sebanor03@gmail.com>
2022-11-24 01:38:53 -0300

    clase 22

---
+++ clase22-react/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import Header from './componentes/layout/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ clase22-react/src/componentes/layout/Header.js
@@ -0,0 +1,6 @@
+const Header = (props)=>{
+    return(
+        <p>hola soy el Header</p>
+    )
+}
+export default Header;
